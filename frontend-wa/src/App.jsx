b1576f8097453316e227700680027db5f585e05a
indrabsudirman <indrabsudirman@gmail.com>
2024-01-28 21:53:29 +0700

    first commit

---
+++ frontend-wa/src/App.jsx
@@ -0,0 +1,12 @@
+import { CallIcon } from "/svg/Call";
+// import CallIcon from "../../frontend/src/svg/Call.js";
+function App() {
+  return (
+    <>
+      <h1 className="text-red-500">Whatsapp Web With AI</h1>
+      <CallIcon />
+    </>
+  );
+}
+
+export default App;
