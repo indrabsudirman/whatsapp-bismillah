0a3eb425b434c36de01c2cb14531f75c3f61b87f
indrabsudirman <indrabsudirman@gmail.com>
2024-01-29 09:16:13 +0700

    Added parse JSON request

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,8 +17,19 @@ if (process.env.NODE_ENV !== "production") {
 //helmet
 app.use(helmet());
 
+//parse JSON req url
+app.use(express.json());
+
+//parse JSON req body
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.send("Hello from server");
+});
+
+//added sample post
+app.post("/test", (req, res) => {
+  res.send(req.body);
 });
 
 export default app;
