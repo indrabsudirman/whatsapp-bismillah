a48b95039b33427e31a44f30241f906823e4ab36
indrabsudirman <indrabsudirman@gmail.com>
2024-01-29 09:00:56 +0700

    Added helmet

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
 import morgan from "morgan";
+import helmet from "helmet";
 
 //dotEnv config
 dotenv.config();
@@ -12,6 +13,9 @@ const app = express();
 if (process.env.NODE_ENV !== "production") {
   app.use(morgan("dev"));
 }
+
+//helmet
+app.use(helmet());
 
 app.get("/", (req, res) => {
   res.send("Hello from server");
