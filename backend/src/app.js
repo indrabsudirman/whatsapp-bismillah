37f78d2f3c7599851bb3e17b0524f5a417cdee8e
indrabsudirman <indrabsudirman@gmail.com>
2024-01-29 08:48:00 +0700

    Add morgan

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,17 @@
+import dotenv from "dotenv";
 import express from "express";
+import morgan from "morgan";
+
+//dotEnv config
+dotenv.config();
 
 //create express app
 const app = express();
+
+//morgan
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"));
+}
 
 app.get("/", (req, res) => {
   res.send("Hello from server");
