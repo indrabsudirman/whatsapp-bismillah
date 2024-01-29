e29ba558d6256e0da3ef4e3de52fd2b5fd3613d7
indrabsudirman <indrabsudirman@gmail.com>
2024-01-29 22:36:36 +0700

    Handle error handling, http error

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,6 +7,7 @@ import cookieParser from "cookie-parser";
 import compression from "compression";
 import fileUpload from "express-fileupload";
 import cors from "cors";
+import createHttpError from "http-errors";
 
 //dotEnv config
 dotenv.config();
@@ -38,9 +39,11 @@ app.use(cookieParser());
 app.use(compression());
 
 //file upload
-app.use(fileUpload({
-  useTempFiles: true,
-}));
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
 
 //cors
 app.use(cors());
@@ -52,6 +55,21 @@ app.get("/", (req, res) => {
 //added sample post
 app.post("/test", (req, res) => {
   res.send(req.body);
+});
+
+app.use(async (req, res, next) => {
+  next(createHttpError.NotFound("This route does not exist."));
+});
+
+//error handling
+app.use(async (err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send({
+    error: {
+      status: err.status || 500,
+      message: err.message,
+    },
+  });
 });
 
 export default app;
