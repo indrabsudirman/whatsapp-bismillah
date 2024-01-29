84743e4aab553cfa4772f9ae148ff19d29bdf48b
indrabsudirman <indrabsudirman@gmail.com>
2024-01-29 22:17:44 +0700

    Added sigterm

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,9 +4,37 @@ import logger from "./configs/logger.config.js";
 //env variables
 const PORT = process.env.PORT || 8000;
 
+let server;
+
 //log the ENV
 console.log(process.env.NODE_ENV);
 
-app.listen(PORT, () => {
+server = app.listen(PORT, () => {
   logger.info(`App is litening at port : ${PORT}...`);
+  console.log("process id is ", process.pid);
+});
+
+//handle server error
+const exitHandler = () => {
+  if (server) {
+    logger.info("Server closed.");
+    process.exit(1);
+  } else {
+    process.exit(1);
+  }
+};
+
+const unexpectedErrorHandler = (error) => {
+  logger.error(error);
+  exitHandler();
+};
+process.on("uncaughtException", unexpectedErrorHandler);
+process.on("unhandledRejection", unexpectedErrorHandler);
+
+//SIGTERM
+process.on("SIGTERM", () => {
+  if (server) {
+    logger.info("Server closed.");
+    process.exit(1);
+  }
 });
