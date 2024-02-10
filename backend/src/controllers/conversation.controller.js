5cdd34d50f08ef1eec6bc783ad232feafe09f767
indrabsudirman <indrabsudirman@gmail.com>
2024-02-10 23:32:44 +0700

    Added get user conversations

--- backend/src/controllers/conversation.controller.js
+++ backend/src/controllers/conversation.controller.js
@@ -4,6 +4,7 @@ import {
   doesConversationExist,
   createConversation,
   populateConversation,
+  getUserConversations,
 } from "../services/conversation.service.js";
 import { findUser } from "../services/user.service.js";
 
@@ -40,6 +41,16 @@ export const createOpenConversation = async (req, res, next) => {
       );
       res.status(200).json(populatedConvo);
     }
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getConversations = async (req, res, next) => {
+  try {
+    const user_id = req.user.userId;
+    const conversations = await getUserConversations(user_id);
+    res.status(200).json(conversations);
   } catch (error) {
     next(error);
   }
--- backend/src/routes/conversation.route.js
+++ backend/src/routes/conversation.route.js
@@ -1,9 +1,13 @@
 import express from "express";
 import trimRequest from "trim-request";
 import authMiddleware from "../middlewares/authMiddleware.js";
-import { createOpenConversation } from "../controllers/conversation.controller.js";
+import {
+  createOpenConversation,
+  getConversations,
+} from "../controllers/conversation.controller.js";
 const router = express.Router();
 
 router.route("/").post(trimRequest.all, authMiddleware, createOpenConversation);
+router.route("/").get(trimRequest.all, authMiddleware, getConversations);
 
 export default router;
