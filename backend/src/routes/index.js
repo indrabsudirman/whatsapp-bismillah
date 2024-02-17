51e7164c1048b5fcdfd51f35d19e3df06d169548
indrabsudirman <indrabsudirman@gmail.com>
2024-02-17 13:09:19 +0700

    Added search users

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,10 +2,12 @@ import express from "express";
 import authRoute from "./auth.route.js";
 import conversationRoutes from "./conversation.route.js";
 import messageRoutes from "./message.route.js";
+import usersRoute from "./user.route.js";
 
 const router = express.Router();
 
 router.use("/auth", authRoute);
+router.use("/user", usersRoute);
 router.use("/conversation", conversationRoutes);
 router.use("/message", messageRoutes);
 
