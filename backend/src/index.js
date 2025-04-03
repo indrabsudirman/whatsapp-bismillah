6cd17148719265baedf43eb8a39574a58dac8e97
indrabsudirman <indrabsudirman@gmail.com>
2025-04-04 06:54:42 +0700

    Updated index js, add log db name

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,21 +1,21 @@
-import mongoose from "mongoose";
-import { Server } from "socket.io";
-import app from "./app.js";
-import logger from "./configs/logger.config.js";
-import SocketServer from "./SocketServer.js";
+import mongoose from 'mongoose'
+import { Server } from 'socket.io'
+import app from './app.js'
+import logger from './configs/logger.config.js'
+import SocketServer from './SocketServer.js'
 
 //env variables
-const { DATABASE_URL } = process.env;
-const PORT = process.env.PORT || 8000;
+const { DATABASE_URL } = process.env
+const PORT = process.env.PORT || 8000
 
 //exit on mongodb error
-mongoose.connection.on("error", (err) => {
-  logger.error(`Mongo DB error ${err}`);
-  process.exit(1);
-});
+mongoose.connection.on('error', (err) => {
+  logger.error(`Mongo DB error ${err}`)
+  process.exit(1)
+})
 
 //mongodb debug mode
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV !== 'production') {
   // mongoose.set("debug", true);
 }
 
@@ -26,18 +26,21 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
-    logger.info("Connected to MongoDB");
-  });
+    logger.info('Connected to MongoDB')
+    logger.info(`Coonnected to database : ${mongoose.connection.name}`)
+    logger.info(`Coonnected to host : ${mongoose.connection.host}`)
+    logger.info(`Coonnected to port : ${mongoose.connection.port}`)
+  })
 
-let server;
+let server
 
 //log the ENV
-logger.info(process.env.NODE_ENV);
+logger.info(process.env.NODE_ENV)
 
 server = app.listen(PORT, () => {
-  logger.info(`App is litening at port : ${PORT}.`);
+  logger.info(`App is litening at port : ${PORT}.`)
   // console.log("process id is ", process.pid);
-});
+})
 
 //socket io
 const io = new Server(server, {
@@ -45,34 +48,34 @@ const io = new Server(server, {
   cors: {
     origin: process.env.CLIENT_ENDPOINT,
   },
-});
+})
 
-io.on("connection", (socket) => {
-  logger.info(`socket io with id ${socket.id} successfully connected`);
-  SocketServer(socket);
-});
+io.on('connection', (socket) => {
+  logger.info(`socket io with id ${socket.id} successfully connected`)
+  SocketServer(socket)
+})
 
 //handle server error
 const exitHandler = () => {
   if (server) {
-    logger.info("Server closed.");
-    process.exit(1);
+    logger.info('Server closed.')
+    process.exit(1)
   } else {
-    process.exit(1);
+    process.exit(1)
   }
-};
+}
 
 const unexpectedErrorHandler = (error) => {
-  logger.error(error);
-  exitHandler();
-};
-process.on("uncaughtException", unexpectedErrorHandler);
-process.on("unhandledRejection", unexpectedErrorHandler);
+  logger.error(error)
+  exitHandler()
+}
+process.on('uncaughtException', unexpectedErrorHandler)
+process.on('unhandledRejection', unexpectedErrorHandler)
 
 //SIGTERM
-process.on("SIGTERM", () => {
+process.on('SIGTERM', () => {
   if (server) {
-    logger.info("Server closed.");
-    process.exit(1);
+    logger.info('Server closed.')
+    process.exit(1)
   }
-});
+})
